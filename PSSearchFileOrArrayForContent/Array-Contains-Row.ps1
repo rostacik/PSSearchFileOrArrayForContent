3fc0209664c6d844178d7725b47edd7dcc359c97
rostacik <rostacik@gmail.com>
2015-04-22 09:56:09 +0200

    just some data

---
+++ PSSearchFileOrArrayForContent/Array-Contains-Row.ps1
@@ -0,0 +1,23 @@
+function Array-Contains-Row {
+	[CmdletBinding()]
+	Param([array]$fileContent, [string]$searchedString)
+
+	Process {
+		#is there any string we search for in any of the rows?
+		[array]$resArray = $fileContent | where { $_ | Select-String -Pattern $searchedString }
+
+		if ($resArray.Count -eq 0) {
+			return $false
+		} else {
+			return $true
+		}
+	}
+}
+
+#array sample
+$inputArr = ('aa', 'bb', 'cd')
+$outputArr = Array-Contains-Row -fileContent $inputArr -searchedString 'aa'
+
+#file sample
+$inputFile = (get-content 'absolute path')
+$outputFile = Array-Contains-Row -fileContent $inputFile -searchedString 'what we search for'
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PSSearchFileOrArrayForContent
-PowerShell snippet to search file or array for content
+PowerShell snippet to search file or array for content.
+
+Small snippet encapsulating search for item in array. Can be also used against file content and search for presence of string in text file.
+
+Sample at the end of the file.
